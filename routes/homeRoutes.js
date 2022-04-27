e6ded5eda425b84b773e028c286fee6a045fbce1
JJerrychan <164768186@qq.com>
2022-04-27 17:14:56 -0400

    Homepage update

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.render('home/home', {login_flag: 'home', username: req.session.user.account})
+    res.render('home/home')
 });
+// router.get('/', async (req, res) => {
+//     res.render('home/home', {login_flag: 'home', username: req.session.user.account})
+// });
 
 module.exports = router;
