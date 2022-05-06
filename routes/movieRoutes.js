d0b4811ad553de87958bc375e591eef4f14c00fc
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2022-05-06 04:59:34 -0400

    cal rate

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -203,16 +203,15 @@ router.post("/comment", async (req, res) => {
   }
 });
 
-
-//calculate rate
 /*
-router.post('/getRate', async (req, res) => {
+//calculate rate
+router.get('/getRate', async (req, res) => {
   try {
-  const movieId = req.body.movieId;
-  const getRate = await commentData.calMovieRate(movieId);
-  const movie = await movieData.getByName(getRate);
+    const movieId = req.body.movieId;
+    const getRate = await commentData.calMovieRate(movieId);
+    const movie = await movieData.getByName(getRate);
 
-  res.render('movie/details', {movie: movie});
+    res.render('movie/details', {movie: movie});
   } catch (e) {
     console.log(e);
     res.status(400).send({
