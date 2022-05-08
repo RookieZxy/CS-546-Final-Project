8fdf637dc7a9af94ed9eb582ad324a25313e94d6
An Sun <ansun3366@gmail.com>
2022-05-08 19:02:11 -0400

    Update filter in type and search page

--- public/js/type.js
+++ public/js/type.js
@@ -31,92 +31,113 @@
         })
     })
 
-    $(document).on('change', 'input[type=radio][name=ratingOptions]', function (){
-        var rating = $(this).val();
-        $('.type-single-movie').each(function(index, element){
-            if ($(element).children('.type-single-movie-rating').text() >= rating){
+    // $(document).on('change', 'input[type=radio][name=ratingOptions]', function (){
+    //     var rating = $(this).val();
+    //     $('.type-single-movie').each(function(index, element){
+    //         if ($(element).children('.type-single-movie-rating').text() >= rating){
+    //             $(element).show();
+    //         }
+    //         else{
+    //             $(element).hide();
+    //         }
+    //     })
+    // })
+
+    // $(document).on('change', 'input[type=radio][name=search-ratingOptions]', function (){
+    //     var rating = $(this).val();
+    //     $('.search-single-movie').each(function(index, element){
+    //         if ($(element).children('.search-single-movie-rating').text() >= rating){
+    //             $(element).show();
+    //         }
+    //         else{
+    //             $(element).hide();
+    //         }
+    //     })
+    // })
+
+    // $(document).on('change', 'input[type=radio][name=runTimeOptions]', function (){
+    //     var choice = $(this).val();
+    //     if (choice == "0"){
+    //         $('.type-single-movie').each(function(index, element){
+    //             if ($(element).children('.type-single-movie-runTime').text() >= 0){
+    //                 $(element).show();
+    //             }
+    //             else{
+    //                 $(element).hide();
+    //             }
+    //         })
+    //     }else if (choice == "1"){
+    //         $('.type-single-movie').each(function(index, element){
+    //             if ($(element).children('.type-single-movie-runTime').text() <= 90){
+    //                 $(element).show();
+    //             }
+    //             else{
+    //                 $(element).hide();
+    //             }
+    //         })
+    //     }else{
+    //         $('.type-single-movie').each(function(index, element){
+    //             if ($(element).children('.type-single-movie-runTime').text() >= 90){
+    //                 $(element).show();
+    //             }
+    //             else{
+    //                 $(element).hide();
+    //             }
+    //         })
+    //     }
+    // })
+
+    // $(document).on('change', 'input[type=radio][name=search-runTimeOptions]', function (){
+    //     var choice = $(this).val();
+    //     if (choice == "0"){
+    //         $('.search-single-movie').each(function(index, element){
+    //             if ($(element).children('.search-single-movie-runTime').text() >= 0){
+    //                 $(element).show();
+    //             }
+    //             else{
+    //                 $(element).hide();
+    //             }
+    //         })
+    //     }else if (choice == "1"){
+    //         $('.search-single-movie').each(function(index, element){
+    //             if ($(element).children('.search-single-movie-runTime').text() <= 90){
+    //                 $(element).show();
+    //             }
+    //             else{
+    //                 $(element).hide();
+    //             }
+    //         })
+    //     }else{
+    //         $('.search-single-movie').each(function(index, element){
+    //             if ($(element).children('.search-single-movie-runTime').text() >= 90){
+    //                 $(element).show();
+    //             }
+    //             else{
+    //                 $(element).hide();
+    //             }
+    //         })
+    //     }
+    // })
+
+
+    $(document).on('change', 'input[type=radio]', function (){
+        var rating = parseFloat($('input[name=ratingOptions]:checked').val());
+        var runtime = parseInt($('input[name=runTimeOptions]:checked').val());
+        var amount = 0;
+        //console.log(rating, runtime);
+        $('.single-movie').each(function(index, element){
+            var tempRating = $(element).children('.single-movie-rating').text();
+            var tempRunTime = $(element).children('.single-movie-runTime').text();
+            //console.log(tempRating, tempRunTime );
+            if (tempRating >= rating && tempRunTime <= runtime){
+                amount++;
                 $(element).show();
             }
             else{
                 $(element).hide();
             }
         })
-    })
-
-    $(document).on('change', 'input[type=radio][name=search-ratingOptions]', function (){
-        var rating = $(this).val();
-        $('.search-single-movie').each(function(index, element){
-            if ($(element).children('.search-single-movie-rating').text() >= rating){
-                $(element).show();
-            }
-            else{
-                $(element).hide();
-            }
-        })
-    })
-
-    $(document).on('change', 'input[type=radio][name=runTimeOptions]', function (){
-        var choice = $(this).val();
-        if (choice == "0"){
-            $('.type-single-movie').each(function(index, element){
-                if ($(element).children('.type-single-movie-runTime').text() >= 0){
-                    $(element).show();
-                }
-                else{
-                    $(element).hide();
-                }
-            })
-        }else if (choice == "1"){
-            $('.type-single-movie').each(function(index, element){
-                if ($(element).children('.type-single-movie-runTime').text() <= 90){
-                    $(element).show();
-                }
-                else{
-                    $(element).hide();
-                }
-            })
-        }else{
-            $('.type-single-movie').each(function(index, element){
-                if ($(element).children('.type-single-movie-runTime').text() >= 90){
-                    $(element).show();
-                }
-                else{
-                    $(element).hide();
-                }
-            })
-        }
-    })
-
-    $(document).on('change', 'input[type=radio][name=search-runTimeOptions]', function (){
-        var choice = $(this).val();
-        if (choice == "0"){
-            $('.search-single-movie').each(function(index, element){
-                if ($(element).children('.search-single-movie-runTime').text() >= 0){
-                    $(element).show();
-                }
-                else{
-                    $(element).hide();
-                }
-            })
-        }else if (choice == "1"){
-            $('.search-single-movie').each(function(index, element){
-                if ($(element).children('.search-single-movie-runTime').text() <= 90){
-                    $(element).show();
-                }
-                else{
-                    $(element).hide();
-                }
-            })
-        }else{
-            $('.search-single-movie').each(function(index, element){
-                if ($(element).children('.search-single-movie-runTime').text() >= 90){
-                    $(element).show();
-                }
-                else{
-                    $(element).hide();
-                }
-            })
-        }
+        //console.log(amount);
     })
 
 })(window.jQuery);
