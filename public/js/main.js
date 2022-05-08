314f3797db0b4ea3fa062db7abd0fa2a52051c55
TangOvO <215491611@qq.com>
2022-05-08 11:24:11 -0400

    11
    1

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,27 @@
 (function ($) {
-	"use strict";
-	$("#userManage").hide();
-	$("#addmovie").hide();
-	$("#invalidList").hide();
-	var requestAdmin = {
-		method: 'GET',
-		url: 'http://localhost:3000/userInfo'
-	}
-	
-	$.ajax(requestAdmin).then((object) => {
-		if (object.isAdmin == true) {
-			$("#userManage").show();
-			$("#invalidList").show();
-		}
-		if (object.username != null) {
-			$("#login").hide();
-			$("#signup").hide();
-			$("#addmovie").show();
-		} else {
-			$("#logout").hide();
-		}
-		if($("#login_flag").val() == 'false'){
-			alert($("#error").val())
-		}
-	});
+  "use strict";
+  $("#userManage").hide();
+  $("#addmovie").hide();
+  $("#invalidList").hide();
+  var requestAdmin = {
+    method: "GET",
+    url: "http://localhost:3000/userInfo",
+  };
 
-
+  $.ajax(requestAdmin).then((object) => {
+    if (object.isAdmin == true) {
+      $("#userManage").show();
+      $("#invalidList").show();
+    }
+    if (object.username != null) {
+      $("#login").hide();
+      $("#signup").hide();
+      $("#addmovie").show();
+    } else {
+      $("#logout").hide();
+    }
+    if ($("#login_flag").val() == "false") {
+      alert($("#error").val());
+    }
+  });
 })(jQuery);
